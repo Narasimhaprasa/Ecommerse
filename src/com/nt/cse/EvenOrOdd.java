9ce9c8677010efcddef3ab802bbfe31c083ea6df
Narasimhaprasad <narasimhaprasad812@gmail.com>
2022-08-11 17:42:41 +0530

    logicak programs

---
+++ src/com/nt/cse/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package com.nt.cse;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+	// Scanner sc = new Scanner(System.in);
+	 int num = 13;
+	 if(num%2==0)
+		 System.out.println(num+" is a even number");
+	 else
+		 System.out.println(num+" is a odd number");
+
+	}
+
+}
